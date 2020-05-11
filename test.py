039aaf47398510da8049f7ff2ae946c5564e81ae
iamsajjad <iamsajjad@mail.ru>
2020-05-11 07:30:41 +0300

    add ignore to test

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 
 from quickping import Quickping
 
-test = Quickping("192.168.0.0", "192.168.0.128", log=True, threads=512)
+test = Quickping("192.168.0.0", "192.168.0.128", ignore=["192.168.0.22"], threads=512, log=True)
 test.active()
+
+print(test.deactiveAddresses)
 print(test.activeAddresses)
 
