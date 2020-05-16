17a629e8a1a0b814c7d8dc23c655ba71ef78655c
iamsajjad <iamsajjad@mail.ru>
2020-05-16 11:04:58 +0300

    add timeToDone and change result display style using click.echo() method

--- cli/cLI.py
+++ cli/cLI.py
@@ -4,6 +4,7 @@ cli/cLI.py
 executable python file of quickping as command-line tools
 """
 
+import time
 import click
 from quickping import Quickping
 
@@ -13,24 +14,33 @@ from quickping import Quickping
 @click.option('-t', '--threads', default=512, type=int, help='number of threads')
 @click.option('-l', '--log', default=False, type=bool, help='display logging')
 def cLI(start, end, threads, log):
-    
+
+    """
+    run python cLI.py --help
+    """
+
+    stimer = time.time()
     try:
         testRange = Quickping(start=start, end=end, ignore=[], threads=threads, log=log)
         testRange.active()
     except expression as identifier:
         raise identifier from None
+    etimer = time.time()
 
-    print('\nActive Addresses\n')
+    click.echo("\nActive Addresses\n")
     for aa in testRange.activeAddresses:
-        print(aa)
+        click.echo(aa)
 
-    informations = """\nStart at : {}\nEnd at : {}\nTotal Addresses : {}\nActive Addresses : {}\nNumber of Threads : {}""".format(
-        testRange.start,
-        testRange.end,
-        len(testRange.addresses),
-        len(testRange.activeAddresses),
-        testRange.threads)
-    click.echo(informations)
+    # time take to process
+    timeToDone = time.strftime("%H:%M:%S", time.gmtime(etimer - stimer))
+    click.echo("")
+    click.echo("Start @           : {}".format(testRange.start))
+    click.echo("End @             : {}".format(testRange.end))
+    click.echo("Total Addresses   : {}".format(len(testRange.addresses)))
+    click.echo("Active Addresses  : {}".format(len(testRange.activeAddresses)))
+    click.echo("Number of Threads : {}".format(testRange.threads))
+    click.echo("Time take to Done : {}".format(timeToDone))
+    click.echo("")
 
 if __name__ == "__main__":
     cLI()
